445d34ebbd5fb6bd0c414b479414685b86a769df
Rajesh Pollepalli <rpollepalli@gmail.com>
2024-01-10 22:04:58 +0000

    Day2 Lab passed

--- src/main/lab.py
+++ src/main/lab.py
@@ -37,9 +37,17 @@ def sampleW2V():
     except KeyError as e:
         print(e)
 
-# TODO: complete the following function to retrieve vector representation of an input word. You should also make sure if it doesn't know the word, it returns -1, it will not crash.(aka, catch the KeyError)
+# TODO: complete the following function to retrieve vector representation of an input word. 
+# You should also make sure if it doesn't know the word, 
+# it returns -1, it will not crash.(aka, catch the KeyError)
 def retrieveVector(input_word):
-    return -1
+    
+    try:
+        vec_input_word = w2v[input_word]
+        return vec_input_word
+    except KeyError as e:
+        print(e)
+        return -1
 """
 We can also do similarity comparisions using the Word2Vec model.
 """
@@ -54,7 +62,8 @@ def sampleSimilarity():
     for w1, w2 in pairs:
         print('%r\t%r\t%.2f' % (w1, w2, w2v.similarity(w1, w2)))
 
-    #It can also retrieve the most similar words to the list of words given. The higher the number, more similar they are in meaning.
+    #It can also retrieve the most similar words to the list of words given. 
+    # The higher the number, more similar they are in meaning.
     print(w2v.most_similar(positive=['cup', 'mug'], topn=5))
 
     # The doesnt_match function takes in a list and finds the least similar word.
@@ -62,19 +71,25 @@ def sampleSimilarity():
 
 
 
-# TODO: Complete the following function so that it calculates the similarity between two words and the similarity index is greater than 0.4
+# TODO: Complete the following function so that it calculates the similarity between two words 
+# and the similarity index is greater than 0.4
 def similarPairExercise():
-    word1 = ""
-    word2 = ""
-    return #return w2v similarity index of word 1 and 2 here
+    word1 = "cap"
+    word2 = "hat"
+    return w2v.similarity(word1, word2)
 
-# TODO: Complete the following function so that w2v model retrieves top 'max_limit' number of the words you provide 
+# TODO: Complete the following function so that w2v model retrieves top 'max_limit' 
+# number of the words you provide 
 def retrieveSimilarWordsExercise(max_limit: int):
-    words = [] #provide your own words that you want the results to be similar to
+    words = ['car','bus','train','airplane'] 
+    #provide your own words that you want the results to be similar to
 
-    return#grab max_limit number of words similar to words appearing in words
+    return w2v.most_similar(positive=words, topn=max_limit)
+    #grab max_limit number of words similar to words appearing in words
 
 # TODO: Complete the following function to return the word that is the least similar in the given list. 
 def w2vDoesntMatchExercise(words: list[str]):
-    
-    return # return the least similar word in the words list 
+    lsw=w2v.doesnt_match(words)
+    print(lsw)
+    return lsw
+    # return the least similar worsd in the words list 
